c51df54b81ec85d03c7b925028bbceef53a0cd76
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-24 21:11:40 +0200

    update App.js to import and render Home component, and add basic CSS reset to index.css"

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <section className={styles.home}>
+        <h1>Welcome Our Product Management System</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
