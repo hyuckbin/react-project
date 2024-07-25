9e0a26ba048220b278792c2458f1762564f67e77
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-25 09:15:29 +0200

    "Added Bootstrap CSS import to App.js and MyNavbar component to Home.jsx"

---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,25 @@
+import styles from "./MyNavbar.module.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const MyNavbar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">New Product</Nav.Link>
+            <Nav.Link href="#link">Product List</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import MyNavbar from "../components/MyNavbar";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.container}>
+      <MyNavbar />
       <section className={styles.home}>
         <h1>Welcome Our Product Management System</h1>
       </section>
