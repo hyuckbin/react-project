dc9cab7060f571f3e2cb69070cf53738e87f4515
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-25 09:57:09 +0200

    "Updated MyNavbar component to use CSS module, added styles for navbar and navlinks, and modified Home page to include buttons with styles"

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,23 +1,29 @@
 import { NavLink } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import styles from "./MyNavbar.module.css";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <NavLink to="/">React-Bootstrap</NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/newproduct">New Product</NavLink>
-            <NavLink to="/productlist">Product List</NavLink>
-            <NavLink to="/about">About</NavLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="lg" className={styles.navbar}>
+      <NavLink to="/">React-Bootstrap</NavLink>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <NavLink to="/" className={styles.navlink}>
+            Home
+          </NavLink>
+          <NavLink to="/newproduct" className={styles.navlink}>
+            New Product
+          </NavLink>
+          <NavLink to="/productlist" className={styles.navlink}>
+            Product List
+          </NavLink>
+          <NavLink to="/about" className={styles.navlink}>
+            About
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,10 @@ const Home = () => {
       <MyNavbar />
       <section className={styles.home}>
         <h1>Welcome Our Product Management System</h1>
+        <div className={styles.buttons}>
+          <button>Add New Product</button>
+          <button>Product List</button>
+        </div>
       </section>
     </div>
   );
