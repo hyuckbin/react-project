c7a9e4048a28206d394aa84776a80bd36faa7d4c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-25 11:48:47 +0200

    "Added axios dependency and implemented API call to fetch products in ProductList component"

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+const url = "https://66a2170f967c89168f1eb98b.mockapi.io/products";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const { data } = await axios.get(url);
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <ProductCard />
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
