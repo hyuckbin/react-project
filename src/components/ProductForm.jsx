c5ca89936a2126b2d023ca18a602cee4c7aa68c4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-25 18:24:16 +0200

    "Added state management to ProductForm component and implemented API calls in NewProduct page"

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -2,7 +2,13 @@ import styles from "./ProductForm.module.css";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import MyNavbar from "./MyNavbar";
+import { useState } from "react";
 const ProductForm = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [url, setUrl] = useState("");
+
   return (
     <div className={styles.container}>
       <MyNavbar />
@@ -14,17 +20,29 @@ const ProductForm = () => {
               <Form.Control
                 type="text"
                 placeholder="Please enter product name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
               />
             </Form.Group>
 
             <Form.Group className="mb-4">
               <Form.Label className="text-white">Product Price</Form.Label>
-              <Form.Control type="number" placeholder="0" />
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={(e) => setPrice(e.target.value)}
+                value={price}
+              />
             </Form.Group>
 
             <Form.Group className="mb-4">
               <Form.Label className="text-white">Product Quantity</Form.Label>
-              <Form.Control type="number" placeholder="0" />
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={(e) => setAmount(e.target.value)}
+                value={amount}
+              />
             </Form.Group>
 
             <Form.Label className="text-white" htmlFor="basic-url">
@@ -34,7 +52,12 @@ const ProductForm = () => {
               <InputGroup.Text id="basic-addon3">
                 https://example.com/
               </InputGroup.Text>
-              <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+              <Form.Control
+                id="basic-url"
+                aria-describedby="basic-addon3"
+                onChange={(e) => setUrl(e.target.value)}
+                value={url}
+              />
             </InputGroup>
           </Form>
           <button type="submit">Save To New Product</button>
