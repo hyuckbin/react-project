7dc1437dc55df3ab16100c6b658fd67e2b86b5d1
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-26 14:54:16 +0200

    add updatProduct component

---
+++ src/pages/UpdateProduct.jsx
@@ -0,0 +1,53 @@
+import styles from "./UpdateProduct.module.css";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import MyNavbar from "./MyNavbar";
+
+const UpdateProduct = () => {
+  return (
+    <div className={styles.container}>
+      <MyNavbar />
+      <section className={styles.productForm}>
+        <div className={styles.formContainer}>
+          <Form>
+            <Form.Group className="mb-4">
+              <Form.Label className="text-white">Product Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Please enter product name"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-4">
+              <Form.Label className="text-white">Product Price</Form.Label>
+              <Form.Control type="number" placeholder="0" required />
+            </Form.Group>
+
+            <Form.Group className="mb-4">
+              <Form.Label className="text-white">Product Quantity</Form.Label>
+              <Form.Control type="number" placeholder="0" required />
+            </Form.Group>
+
+            <Form.Label className="text-white" htmlFor="basic-url">
+              Product Image
+            </Form.Label>
+            <InputGroup className="mb-4">
+              <InputGroup.Text id="basic-addon3">
+                https://example.com/
+              </InputGroup.Text>
+              <Form.Control
+                id="basic-url"
+                aria-describedby="basic-addon3"
+                required
+              />
+            </InputGroup>
+          </Form>
+          <button type="submit">Save To New Product</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default UpdateProduct;
